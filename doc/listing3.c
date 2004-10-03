3e5ee072fe4e8a13b8fa763d4c11c74dcae63626
Martin Hedenfalk <martin@bzero.se>
2004-10-03 19:03:25 +0000

    added code listings for the tutorial

---
+++ doc/listing3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <confuse.h>
+
+int main(void)
+{
+    cfg_opt_t opts[] =
+    {
+        CFG_STR("target", "World", CFGF_NONE),
+        CFG_INT("repeat", 1, CFGF_NONE),
+        CFG_END()
+    };
+    cfg_t *cfg;
+    int repeat;
+
+    cfg = cfg_init(opts, CFGF_NONE);
+    cfg_parse(cfg, "hello.conf");
+
+    repeat = cfg_getint(cfg, "repeat");
+    while(repeat--)
+        printf("Hello, %s!\n", cfg_getstr(cfg, "target"));
+
+    cfg_free(cfg);
+    return 0;
+}
+
---
+++ doc/listing4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <confuse.h>
+
+int main(void)
+{
+    cfg_opt_t opts[] =
+    {
+        CFG_STR_LIST("targets", "{World}", CFGF_NONE),
+        CFG_INT("repeat", 1, CFGF_NONE),
+        CFG_END()
+    };
+    cfg_t *cfg;
+    int repeat;
+    int i;
+
+    cfg = cfg_init(opts, CFGF_NONE);
+    cfg_parse(cfg, "hello.conf");
+
+    repeat = cfg_getint(cfg, "repeat");
+    while(repeat--)
+    {
+        printf("Hello");
+        for(i = 0; i < cfg_size(cfg, "targets"); i++)
+            printf(", %s", cfg_getnstr(cfg, "targets", i));
+        printf("!\n");
+    }
+
+    cfg_free(cfg);
+    return 0;
+}
+
