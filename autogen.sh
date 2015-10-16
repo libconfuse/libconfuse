c13093ab09931132a3b3662e45df2b79a6bd9d9e
Joachim Nilsson <troglobit@gmail.com>
2015-10-16 17:06:19 +0200

    Use autoreconf instead of calling all autotools manually.
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- autogen.sh
+++ autogen.sh
@@ -1,29 +1,8 @@
 #!/bin/sh
+# (Re)Generate configure script and all build related files for i18n
 
-echo -n "Running autopoint..."
-autopoint && echo " done"
+autoreconf -W portability -vism >/dev/null
+
+# Cleanup
 rm -f ABOUT-NLS
-
-echo -n "Running aclocal..."
-aclocal -I m4 || exit
-echo " done"
-
-echo -n "Running autoconf..."
-autoconf || exit
-echo " done"
-
-echo -n "Running autoheader..."
-autoheader || exit
-echo " done"
-
-echo -n "Running libtoolize..."
-libtoolize --automake || glibtoolize --automake || exit
-echo " done"
-
-echo -n "Running automake..."
-automake --add-missing || exit
-echo " done"
-
-echo "Running configure $*..."
-./configure --enable-maintainer-mode $*
 
