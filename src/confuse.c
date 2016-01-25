239712efdb56e49467a24820a02498d866351981
Joachim Nilsson <troglobit@gmail.com>
2016-01-25 11:56:36 +0100

    cfg_include(): Mark 'opt' as unused in this example to prevent warnings
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- src/confuse.c
+++ src/confuse.c
@@ -1539,12 +1539,13 @@ DLLIMPORT int cfg_free(cfg_t *cfg)
 
 DLLIMPORT int cfg_include(cfg_t *cfg, cfg_opt_t *opt, int argc, const char **argv)
 {
+	(void)opt;		/* Unused in this predefined include FUNC */
+
 	if (!cfg || !argv) {
 		errno = EINVAL;
 		return CFG_FAIL;
 	}
 
-	opt = NULL;
 	if (argc != 1) {
 		cfg_error(cfg, _("wrong number of arguments to cfg_include()"));
 		return 1;
