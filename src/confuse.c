bf6ba81c522d646316d979c7173ab6f231c6eb33
Joachim Nilsson <troglobit@gmail.com>
2016-02-20 09:59:53 +0100

    call_function(): Check arguments before dereferencing them
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- src/confuse.c
+++ src/confuse.c
@@ -987,6 +987,11 @@ static int call_function(cfg_t *cfg, cfg_opt_t *opt, cfg_opt_t *funcopt)
 	const char **argv;
 	unsigned int i;
 
+	if (!cfg || !opt ||!funcopt) {
+		errno = EINVAL;
+		return CFG_FAIL;
+	}
+		
 	/*
 	 * create am argv string vector and call the registered function
 	 */
