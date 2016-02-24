1c377793be58c5085c95f1ab1e818f8c129b7fba
Joachim Nilsson <troglobit@gmail.com>
2016-02-24 18:21:00 +0100

    Fix warning for deprecated AM_PROG_MKDIR_P macro in AM_GNU_GETTEXT
    While running autoreconf it warns for a deprecated construct that
    comes from our hard coded version of gettext:
    
    configure.ac:27: warning: The 'AM_PROG_MKDIR_P' macro is deprecated, and its use is discouraged.
    configure.ac:27: You should use the Autoconf-provided 'AC_PROG_MKDIR_P' macro instead,
    configure.ac:27: and use '$(MKDIR_P)' instead of '$(mkdir_p)'in your Makefile.am files.
    
    This patch bumps the gettext version to 0.18.3, which is included
    in the latest Ubuntu and Debian stable releases.
    
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # (Re)Generate configure script and all build related files for i18n
 
-autoreconf -W portability -vism >/dev/null
+autoreconf -W portability -vism --force >/dev/null
 
 # Cleanup
 rm -f ABOUT-NLS
