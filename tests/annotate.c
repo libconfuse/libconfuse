61009ef20e9bbd654c096c90a246e1a851cada86
Joachim Nilsson <troglobit@gmail.com>
2017-05-24 11:01:44 +0200

    Add `CFGF_COMMENTS` flag to enable option annotation support
    With this patch the previous support for option annotation, as well as
    reading/writing comments from/to a file, is conditionalized.  A user
    must explicitly set this flag to enable the feature.
    
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- tests/annotate.c
+++ tests/annotate.c
@@ -3,6 +3,7 @@
 
 int main(void)
 {
+	char *comment;
 	char *expect = "A comment we think will go with this option";
 	cfg_t *cfg;
 	cfg_opt_t *opt;
@@ -18,13 +19,16 @@ int main(void)
 		CFG_END()
 	};
 
-	cfg = cfg_init(opts, CFGF_NONE);
+	cfg = cfg_init(opts, CFGF_COMMENT);
 	fail_unless(cfg != NULL);
 
 	fail_unless(cfg_parse(cfg, SRC_DIR "/annotate.conf") == CFG_SUCCESS);
 
 	opt = cfg_getopt(cfg, "section|option");
-	fail_unless(strcmp(cfg_opt_getcomment(opt), expect) == 0);
+	fail_unless(opt != NULL);
+	comment = cfg_opt_getcomment(opt);
+	fail_unless(comment != NULL);
+	fail_unless(strcmp(comment, expect) == 0);
 
 	fail_unless(cfg_opt_setcomment(opt, "But what's the worst poetry in the universe?") == CFG_SUCCESS);
 
