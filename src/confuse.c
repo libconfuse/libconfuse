b494c622762f1bf19ac1365ea29ee1bb11a5e1ed
Joachim Nilsson <troglobit@gmail.com>
2020-06-21 14:06:14 +0200

    Minor refactor, instead of else break; break early
    This patch looks more complicated than it really is.  In essence, it
    changes the `if (len) { ... } else break;` statement into the simpler
    form `if (!len) break;` and adjusts the indentation and variable decl.
    
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- src/confuse.c
+++ src/confuse.c
@@ -267,69 +267,70 @@ static cfg_opt_t *cfg_getopt_secidx(cfg_t *cfg, const char *name,
 	}
 
 	while (name && *name) {
+		char *title = NULL;
+		long int i = -1;
 		char *secname;
-		size_t len = strcspn(name, "|=");
+		size_t len;
 
+		len = strcspn(name, "|=");
 		if (!index && name[len] == 0 /*len == strlen(name) */ )
 			/* no more subsections */
 			break;
 
-		if (len) {
-			char *title = NULL;
-			long int i = -1;
-
-			secname = strndup(name, len);
-			if (!secname)
-				return NULL;
-
-			do {
-				char *endptr;
-
-				opt = cfg_getopt_leaf(sec, secname);
-				if (!opt || opt->type != CFGT_SEC) {
-					opt = NULL;
-					break;
-				}
-				if (name[len] != '=') {
-					/* non-multi, and backwards compat */
-					i = 0;
-					break;
-				}
-				if (!is_set(CFGF_MULTI, opt->flags))
-					break;
-				name += len + 1;
-				title = parse_title(name, &len);
-				if (!title)
-					break;
-				if (is_set(CFGF_TITLE, opt->flags)) {
-					i = cfg_opt_gettsecidx(opt, title);
-					break;
-				}
-
-				i = strtol(title, &endptr, 0);
-				if (*endptr != '\0')
-					i = -1;
-			} while(0);
-
-			if (index)
-				*index = i;
-
-			sec = i >= 0 ? cfg_opt_getnsec(opt, i) : NULL;
-			if (!sec && !is_set(CFGF_IGNORE_UNKNOWN, cfg->flags)) {
-				if (opt && !is_set(CFGF_MULTI, opt->flags))
-					cfg_error(cfg, _("no such option '%s'"), secname);
-				else if (title)
-					cfg_error(cfg, _("no sub-section '%s' in '%s'"), title, secname);
-				else
-					cfg_error(cfg, _("no sub-section title/index for '%s'"), secname);
-			}
-			free(secname);
-			if (title)
-				free(title);
-			if (!sec)
-				return NULL;
-		} else
+		if (!len)
 			break;
+
+		secname = strndup(name, len);
+		if (!secname)
+			return NULL;
+
+		do {
+			char *endptr;
+
+			opt = cfg_getopt_leaf(sec, secname);
+			if (!opt || opt->type != CFGT_SEC) {
+				opt = NULL;
+				break;
+			}
+			if (name[len] != '=') {
+				/* non-multi, and backwards compat */
+				i = 0;
+				break;
+			}
+			if (!is_set(CFGF_MULTI, opt->flags))
+				break;
+			name += len + 1;
+			title = parse_title(name, &len);
+			if (!title)
+				break;
+			if (is_set(CFGF_TITLE, opt->flags)) {
+				i = cfg_opt_gettsecidx(opt, title);
+				break;
+			}
+
+			i = strtol(title, &endptr, 0);
+			if (*endptr != '\0')
+				i = -1;
+		} while(0);
+
+		if (index)
+			*index = i;
+
+		sec = i >= 0 ? cfg_opt_getnsec(opt, i) : NULL;
+		if (!sec && !is_set(CFGF_IGNORE_UNKNOWN, cfg->flags)) {
+			if (opt && !is_set(CFGF_MULTI, opt->flags))
+				cfg_error(cfg, _("no such option '%s'"), secname);
+			else if (title)
+				cfg_error(cfg, _("no sub-section '%s' in '%s'"), title, secname);
+			else
+				cfg_error(cfg, _("no sub-section title/index for '%s'"), secname);
+		}
+
+		free(secname);
+		if (title)
+			free(title);
+		if (!sec)
+			return NULL;
 
 		name += len;
 		name += strspn(name, "|");
